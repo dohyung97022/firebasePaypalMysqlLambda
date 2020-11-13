d4694c02565e91f15dec3469cd4b6bc74212a847
Doe <dohyung97022@gmail.com>
2020-11-13 18:05:55 +0900

    done!!!!!

--- gofiles/main.go
+++ gofiles/main.go
@@ -13,27 +13,52 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	IDTokenStr, err := server.getStr.header("IDToken")
 	if err!=nil{
 		logger.Printf(err.Error())
-		server.execStr.respond("Need IDToken in header GET request")
+		server.execStr.respond(err.Error())
 		return
 	}
 	context := r.Context()
 	firebase, err := newFirebase(&context)
 	if err!=nil{
 		logger.Printf(err.Error())
-		server.execStr.respond("Firebase could not be initiated")
+		server.execStr.respond(err.Error())
 		return
 	}
 	firebaseToken, err := firebase.getToken.fromTokenStr(IDTokenStr)
 	if err!=nil{
 		logger.Printf(err.Error())
-		server.execStr.respond("firebaseToken is not valid")
+		server.execStr.respond(err.Error())
+		return
+	}
+	mysql, err := newMysql()
+	if err!=nil{
+		logger.Printf(err.Error())
+		server.execStr.respond(err.Error())
 		return
 	}
 	UID := firebase.getStr.UIDFromToken(firebaseToken)
-	
-	newMysql()
-
-	//아래 줄은 보안상 나중에 꼭 삭제하기!!!!!
-	server.setStr.header("Access-Control-Allow-Origin","*")
-	server.execStr.respond("We have ran out of youtube api quotas. Please try again tomorrow.")
+	paymentID, err := mysql.getStr.paymentIDFromUID(UID)
+	if err!=nil{
+		logger.Printf(err.Error())
+		server.execStr.respond(err.Error())
+		return
+	}
+	paypal, err := newPaypal()
+	accessToken, err := paypal.getStr.accessToken()
+	if err!=nil{
+		logger.Printf(err.Error())
+		server.execStr.respond(err.Error())
+		return
+	}
+	varifiedPayment, err := paypal.getBool.varifyPaymentFromPaymentID(paymentID,accessToken)
+	if err!=nil{
+		logger.Printf(err.Error())
+		server.execStr.respond(err.Error())
+		return
+	}
+	if varifiedPayment{
+		//아래 줄은 보안상 나중에 꼭 삭제하기!!!!! cors!!!!
+		server.setStr.header("Access-Control-Allow-Origin","*")
+		//all is varified. Respond and give result!!!!
+		server.execStr.respond("results")	
+	}
 }
